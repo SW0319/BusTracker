c572163e72ad7f79e618739d94ba4c4068f4fd74
anjihun <solutionwalker0319@gmail.com>
2024-01-22 20:18:57 +0900

    Initial Commit

---
+++ BusTracker/BusTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTrackerApp.swift
+//  BusTracker
+//
+//  Created by 안지훈 on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
